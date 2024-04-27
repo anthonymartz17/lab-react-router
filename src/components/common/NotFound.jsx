34f797eaf3b71941843ad6846d043192d4aad998
Antonio Martinez <antonio.fr.martinezc@hotmail.com>
2024-04-27 17:03:57 -0400

    added 404 page

---
+++ src/components/common/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./NotFound.css"
+
+export default function notFound() {
+  return (
+    <div className='container'>
+      {/* <p>Page not found</p> */}
+      <img src="https://www.shutterstock.com/image-vector/black-cat-404-error-warning-260nw-1710063088.jpg" alt="cat img for not found" />
+    </div>
+  )
+}
